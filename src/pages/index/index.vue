e897b4dd1b2baa036def618490034c56a82f3c99
leo <leolichuan@qq.com>
2025-06-06 11:07:55 +0800

    feat: 依赖升级

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,18 +5,26 @@
       <nut-button @click="Taro.navigateTo({ url: '/pages/pageB/index?id=1' })">去PageB</nut-button>
     </nut-space>
     <nut-toast v-model:visible="show" msg="这是首页" />
-    <span :class="styles.redText">这是cssModule测试</span>
+    <span :class="$style.redText">这是cssModule测试</span>
   </view>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import styles from './index.module.scss'
 import Taro from '@tarojs/taro'
+
+definePageConfig({
+  navigationBarTitleText: '首页',
+})
 
 const show = ref(false)
 const onClick = () => {
   show.value = true
 }
 </script>
-<style module lang="scss"></style>
+
+<style module lang="scss">
+.redText {
+  color: var(--color-danger);
+}
+</style>
--- src/pages/pageB/index.vue
+++ src/pages/pageB/index.vue
@@ -3,7 +3,7 @@
     <nut-cell title="路由参数" :desc="instance.router.params.id"></nut-cell>
     <demo-cmp></demo-cmp>
     <nut-button @click="onTryRequest">发起请求</nut-button>
-    <span :class="styles.redText">这是cssModule测试</span>
+    <span :class="$style.greenText">这是cssModule测试</span>
   </view>
 </template>
 
@@ -11,9 +11,11 @@
 import Taro from '@tarojs/taro'
 import DemoCmp from '@/components/DemoCmp/index.vue'
 import { getStart } from '@/apis/boxApi'
-import styles from './index.module.scss'
 
 const instance = Taro.getCurrentInstance()
+definePageConfig({
+  navigationBarTitleText: 'PageB',
+})
 
 function onTryRequest() {
   getStart().then((res) => {
@@ -21,3 +23,9 @@ function onTryRequest() {
   })
 }
 </script>
+
+<style lang="scss" module>
+.greenText {
+  color: var(--color-success);
+}
+</style>
